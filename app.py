6c21b94643cd2e2bc29762b25e350cc716e42d32
Ebed Rafael Mendez Lozano <ebed.ml32@gmail.com>
2024-03-06 17:24:15 -0600

    API_Xamarin

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, jsonify
+import mysql.connector
+import hashlib
+
+app = Flask(__name__)
+
+# Configuración de la base de datos
+db_config = {
+    'host': 'tu_host',
+    'user': 'tu_usuario',
+    'password': 'tu_contraseña',
+    'database': 'tu_base_de_datos'
+}
+
+# Endpoint para registrar usuarios
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.json
+    nombre_usuario = data['nombre_usuario']
+    correo_electronico = data['correo_electronico']
+    contrasena = data['contrasena']
+    
+    # Aquí deberías implementar un mejor mecanismo de cifrado de contraseñas
+    hash_contrasena = hashlib.sha256(contrasena.encode('utf-8')).hexdigest()
+    
+    try:
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor()
+        query = "INSERT INTO Usuarios (NombreUsuario, CorreoElectronico, Contrasena) VALUES (%s, %s, %s)"
+        cursor.execute(query, (nombre_usuario, correo_electronico, hash_contrasena))
+        conn.commit()
+    except mysql.connector.Error as e:
+        return jsonify({'error': str(e)}), 500
+    finally:
+        cursor.close()
+        conn.close()
+    
+    return jsonify({'message': 'Usuario registrado con éxito'}), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
