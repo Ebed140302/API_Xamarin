8c0952e9c63b318f34a87ed235a20b2e060eb904
Ebed Rafael Mendez Lozano <ebed.ml32@gmail.com>
2024-03-06 18:16:23 -0600

    .

--- app.py
+++ app.py
@@ -6,37 +6,39 @@ app = Flask(__name__)
 
 # Configuración de la base de datos
 db_config = {
-    'host': 'tu_host',
-    'user': 'tu_usuario',
-    'password': 'tu_contraseña',
-    'database': 'tu_base_de_datos'
+    'host': 'btnsdrtwj5nxrypwcudz-mysql.services.clever-cloud.com',
+    'user': 'uwec7nhlqoobwbmh',
+    'password': 'L2sdCH6IMHjcw9bbS4AO',
+    'database': 'btnsdrtwj5nxrypwcudz'
 }
+
 
 # Endpoint para registrar usuarios
 @app.route('/register', methods=['POST'])
 def register():
-    data = request.json
-    nombre_usuario = data['nombre_usuario']
-    correo_electronico = data['correo_electronico']
-    contrasena = data['contrasena']
-    
-    # Aquí deberías implementar un mejor mecanismo de cifrado de contraseñas
-    hash_contrasena = hashlib.sha256(contrasena.encode('utf-8')).hexdigest()
-    
-    try:
-        conn = mysql.connector.connect(**db_config)
-        cursor = conn.cursor()
-        query = "INSERT INTO Usuarios (NombreUsuario, CorreoElectronico, Contrasena) VALUES (%s, %s, %s)"
-        cursor.execute(query, (nombre_usuario, correo_electronico, hash_contrasena))
-        conn.commit()
-    except mysql.connector.Error as e:
-        return jsonify({'error': str(e)}), 500
-    finally:
-        cursor.close()
-        conn.close()
-    
-    return jsonify({'message': 'Usuario registrado con éxito'}), 201
+  data = request.json
+  nombre_usuario = data['nombre_usuario']
+  correo_electronico = data['correo_electronico']
+  contrasena = data['contrasena']
+
+  # Aquí deberías implementar un mejor mecanismo de cifrado de contraseñas
+  hash_contrasena = hashlib.sha256(contrasena.encode('utf-8')).hexdigest()
+
+  try:
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+    query = "INSERT INTO Usuarios (NombreUsuario, CorreoElectronico, Contrasena) VALUES (%s, %s, %s)"
+    cursor.execute(query,
+                   (nombre_usuario, correo_electronico, hash_contrasena))
+    conn.commit()
+  except mysql.connector.Error as e:
+    return jsonify({'error': str(e)}), 500
+  finally:
+    cursor.close()
+    conn.close()
+
+  return jsonify({'message': 'Usuario registrado con éxito'}), 201
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+  app.run(debug=True)
